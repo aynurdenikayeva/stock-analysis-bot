22a4d64c967c9e7b65b06964fa524d0bb6578cec
Aynur <113580226+aynurdenikayeva@users.noreply.github.com>
2026-09-23 22:59:03 +0400

    Refactor project structure to root directory

--- stock-analyzer/src/main/java/com/aynur/stockbot/repository/PortfolioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aynur.stockbot.repository;
-
-import com.aynur.stockbot.model.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-}
